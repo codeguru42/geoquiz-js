9f3c40ebbcc721c990be734920526917a452468d
Code-Apprentice <codeguru42@gmail.com>
2022-04-22 17:14:54 -0600

    Draw outline of Ireland

--- js/index.js
+++ js/index.js
@@ -21,19 +21,31 @@ function initMap() {
         }
     ];
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 5,
         center: startLocation,
         styles: styles
     });
 
-    const coordinates = countries.features[0].geometry.coordinates;
+    const coordinates = countries.features[107].geometry.coordinates;
     console.log("coordinates", coordinates)
     const border = coordinates.map(coords => coords.map(coordinatesToLatLng));
     console.log("border", border)
+    const polygon = new google.maps.Polygon({
+        paths: border[6],
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+    });
+    const center = border[6][0][0];
+    console.log("center", center)
+    map.setCenter(center)
+    polygon.setMap(map);
 }
 
 function coordinatesToLatLng(coordinates) {
-    return coordinates.map(([lat, lng]) => ({
+    return coordinates.map(([lng, lat]) => ({
         lat,
         lng
     }));
