7b5b56bacdad0fc6b8f833bb9c03849daefbb1bc
Code-Apprentice <codeguru42@gmail.com>
2022-04-22 18:06:47 -0600

    Refactor drawCountry()

--- js/events.js
+++ js/events.js
@@ -1,6 +1,44 @@
+import { countries } from './countries.js';
+
+function drawCountry(map, countryIndex) {
+    const coordinates = countries.features[countryIndex].geometry.coordinates;
+    const borders = coordinates.map(coords => coords.map(coordinatesToLatLng));
+    drawPolygons(map, borders);
+    const center = borders[0][0][0];
+    console.log("center", center)
+    map.setCenter(center)
+}
+
+function drawPolygons(map, borders) {
+    borders.forEach(border => {
+        const polygon = new google.maps.Polygon({
+            paths: border,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+        });
+        polygon.setMap(map);
+    });
+}
+
+function coordinatesToLatLng(coordinates) {
+    return coordinates.map(([lng, lat]) => ({
+        lat,
+        lng
+    }));
+}
+
 function nextCountry() {
     console.log("next country")
 }
 
 document.getElementById("next-country")
     .addEventListener('click', nextCountry);
+document.addEventListener('load', () => {
+    console.log('load');
+    drawCountry(currCountryIndex);
+});
+
+export { drawCountry }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { countries } from './countries.js';
+import { drawCountry } from './events.js';
 
 function initMap() {
     const startLocation = {
@@ -25,38 +25,7 @@ function initMap() {
         center: startLocation,
         styles: styles
     });
-
-    const coordinates = countries.features[107].geometry.coordinates;
-    console.log("coordinates", coordinates)
-    const borders = coordinates.map(coords => coords.map(coordinatesToLatLng));
-    console.log("border", borders)
-    drawPolygons(map, borders);
-    const center = borders[6][0][0];
-    console.log("center", center)
-    map.setCenter(center)
-}
-
-function drawPolygons(map, borders) {
-    borders.forEach(border => {
-        const polygon = new google.maps.Polygon({
-            paths: border,
-            strokeColor: "#FF0000",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#FF0000",
-            fillOpacity: 0.35,
-        });
-        polygon.setMap(map);
-    });
-}
-
-function coordinatesToLatLng(coordinates) {
-    return coordinates.map(([lng, lat]) => ({
-        lat,
-        lng
-    }));
+    drawCountry(map, 0);
 }
 
 window.initMap = initMap;
-
-console.log("countries", countries)
